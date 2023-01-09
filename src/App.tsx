48503a0d98a029e4a84efb56591179824b090328
Bernardo Nunes <bmn02@outlook.com>
2023-01-09 16:32:03 -0300

    feat: ticTacToe

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState(1);
 
+  const handleClickIndex = (index: number) => {
+    if (gameData[index] !== 0) {
+      return;
+    }
+
+    setGameData((prevState) => {
+      const newGameData = [...prevState];
+      newGameData[index] = turn;
+      return newGameData;
+    });
+
+    setTurn((prevState) => (prevState === 1 ? 2 : 1));
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="board-game">
+        {gameData.map((value, index) => (
+          <span key={index} onClick={() => handleClickIndex(index)}>
+            {value === 1 && "X"}
+            {value === 2 && "O"}
+          </span>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
